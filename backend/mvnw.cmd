de9a00029fd2a42f985b1eb42d4c7bc50f2e30ff
smaison01 <smaison01@gmail.com>
2026-05-28 17:20:56 +0900

    fix: cmd 파일 한글 제거 및 mvnw.cmd Maven 자동 다운로드로 교체

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -1,41 +1,16 @@
-@REM ----------------------------------------------------------------------------
-@REM Licensed to the Apache Software Foundation (ASF)
-@REM Maven Wrapper startup batch script
-@REM ----------------------------------------------------------------------------
+@echo off
+set MAVEN_DIR=%~dp0.mvn\wrapper\apache-maven
+set MAVEN_ZIP=%~dp0.mvn\wrapper\apache-maven.zip
+set MAVEN_URL=https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.9.9/apache-maven-3.9.9-bin.zip
 
-@IF "%__MVNW_ARG0_NAME__%"=="" (SET __MVNW_ARG0_NAME__=%~nx0)
-@SET %%i=
-@FOR %%i IN ("%~dp0.") DO @SET __MVNW_LAUNCHER_PROJECT_BASEDIR__=%%~fi
-
-@SET __MVNW_CMD__=
-@SET __MVNW_ERROR__=
-@SET __MVNW_MAVEN_MANAGER_URL__=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.3.2/maven-wrapper-3.3.2.jar
-
-@SET __MVNW_PROPERTIES_FILE_DIR__=%__MVNW_LAUNCHER_PROJECT_BASEDIR__%\.mvn\wrapper
-
-@IF NOT EXIST "%__MVNW_PROPERTIES_FILE_DIR__%\maven-wrapper.properties" (
-    @ECHO Could not find %__MVNW_PROPERTIES_FILE_DIR__%\maven-wrapper.properties, downloading...
+if not exist "%MAVEN_DIR%\bin\mvn.cmd" (
+    echo Downloading Maven 3.9.9...
+    powershell -Command "Invoke-WebRequest -Uri '%MAVEN_URL%' -OutFile '%MAVEN_ZIP%'"
+    powershell -Command "Expand-Archive -Path '%MAVEN_ZIP%' -DestinationPath '%~dp0.mvn\wrapper\tmp' -Force"
+    move "%~dp0.mvn\wrapper\tmp\apache-maven-3.9.9" "%MAVEN_DIR%"
+    rmdir "%~dp0.mvn\wrapper\tmp"
+    del "%MAVEN_ZIP%"
+    echo Maven download complete.
 )
 
-@SET MAVEN_PROJECTBASEDIR=%__MVNW_LAUNCHER_PROJECT_BASEDIR__%
-
-@SET JAVA_HOME_CANDIDATES=
-@FOR /F "usebackq tokens=*" %%i IN (`where java 2^>NUL`) DO @SET JAVA_HOME_CANDIDATES=%%~dpi..
-
-@IF "%JAVA_HOME%"=="" (
-    @FOR %%i IN (%JAVA_HOME_CANDIDATES%) DO @IF NOT "%JAVA_HOME%"=="" @GOTO :mvn_find_jvm_done
-    @FOR %%i IN (%JAVA_HOME_CANDIDATES%) DO @SET JAVA_HOME=%%i
-    :mvn_find_jvm_done
-)
-
-@SET JAVA_EXECUTABLE=%JAVA_HOME%\bin\java.exe
-@IF NOT EXIST "%JAVA_EXECUTABLE%" SET JAVA_EXECUTABLE=java
-
-@IF NOT EXIST "%__MVNW_PROPERTIES_FILE_DIR__%\maven-wrapper.jar" (
-    "%JAVA_EXECUTABLE%" -cp "%__MVNW_PROPERTIES_FILE_DIR__%\maven-wrapper.jar" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" org.apache.maven.wrapper.MavenWrapperMain %* 2>NUL
-    IF ERRORLEVEL 1 (
-        "%JAVA_EXECUTABLE%" -jar "%__MVNW_PROPERTIES_FILE_DIR__%\maven-wrapper.jar" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" %*
-    )
-) ELSE (
-    "%JAVA_EXECUTABLE%" -jar "%__MVNW_PROPERTIES_FILE_DIR__%\maven-wrapper.jar" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" %*
-)
+"%MAVEN_DIR%\bin\mvn.cmd" %*
